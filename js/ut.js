13dd543618d656c3a159e23b655b96fa6e221957
Maria Velazquez <marivel.del.97@gmail.com>
2023-09-30 23:41:28 -0600

    Cree un nuevo repositorio con la tarea de la lista de numeros con su index.html y su js

---
+++ js/ut.js
@@ -0,0 +1,21 @@
+
+// Se crea una lista que contenga los números del 1 al 100
+for (let i = 1; i <= 100; i++) {
+    // Verifica si el número es divisible entre 3 y 5
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("Ingeniería UTTC");
+    }
+    // Verifica si el número es divisible entre 3
+    else if (i % 3 === 0) {
+      console.log("UTTC");
+    }
+    // Verifica aqui si el número es divisible entre 5
+    else if (i % 5 === 0) {
+      console.log("Ingeniería");
+    }
+    // Si no cumple ninguna de las condiciones anteriores, imprimir el número
+    else {
+      console.log(i);
+    }
+  }
+  
